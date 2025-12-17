d02265d293a071827dbbf5ec123d6f8e4054a498
Athanas Mochama <athanasmochama253@gmail.com>
2025-12-17 10:47:04 +0300

    Complete timer functions - countdownTimer and delayedReminder

---
+++ src/delayedReminder.js
@@ -0,0 +1,12 @@
+
+// delayedReminder function
+function delayedReminder(message, delay) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log(message);
+        resolve();
+      }, delay);
+    });
+  }
+  
+  module.exports = delayedReminder;
